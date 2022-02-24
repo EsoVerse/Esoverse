48224faf7c4f2556fb1739dacb4ad56bfd7aa0ba
Abhijit Prakash Bhatnagar <bhatnaa@amazon.com>
2022-02-24 12:46:35 -0800

    Changes - ALOT

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import { FaTwitter, FaFacebook, FaDiscord } from 'react-icons/fa';
 import Nav from './Nav';
-import Mint from './Mint';
 import WebFont from 'webfontloader';
 import './App.css';
 import nfts from './nfts.json'
@@ -14,7 +13,7 @@ function App() {
   useEffect(() => {
     WebFont.load({
       google: {
-        families: ['Roboto', 'Rum Raisin']
+        families: ['Quicksand', 'Lato']
       }
     });
   }, []);
@@ -23,28 +22,28 @@ function App() {
   let roadmap = [
     {
       step: "01",
-      title: "Launch Roadmap",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      title: "A series of TunnelNecks Launch!",
+      text: " Identify what your version of TunnelNeck will look like! A series of collections from EsoVerse have started to launch starting with - Frocode. Frocodes can be minted and are being revealed to the real world. However, in Esoverse, there exist countless number of esoteric necks to interact, become, play and submerge yourself in! A universe where your if's and but's can turn into Ahaa's! Come join the esoverse community to embrace your wierd within."
     },
     {
       step: "02",
-      title: "Presale",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      title: "Prior access to Tunnel Necks",
+      text: "Do you want priority access to the TunnelNecks by following some simple steps? Join our Discord community to gain exclusive access to Esoverse. You will also get an opportunity to enter in the TunnelNeck Whitelist."
     },
     {
       step: "03",
-      title: "Open Minting",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      title: "Airdropping TunnelNecks",
+      text: "Rare cards of EsoVerse will be distributed for the first 100 members to join our community! "
     },
     {
       step: "04",
-      title: "Giveaways",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      title: "Mint TunnelNecks",
+      text: "Surprise drop in April 2022. You get to decide, what you want on your version of TunnelNeck. Additionally, it doubles as an exclusive access membership card for soon-to-be-lauched EsoVerse!"
     },
     {
       step: "05",
-      title: "Something Great",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      title: "Embrace your esoteric",
+      text: "A first look at what esoverse will look like"
     },
   ]
 
@@ -52,71 +51,55 @@ function App() {
 
     <div className="App">
       {/* https://source.unsplash.com/random */}
-      <div className="bg-gradient-to-r from-blue-400 via-purple-500 to-blue-800">
+      <div className="bg-gradient-to-r from-red-200 via-red-300 to-red-200">
         <div className="containerr px-10 flex mx-auto w-full py-5">
           {/* eslint-disable-next-line */}
           <a name="Top" />
-          <div className="w-12">
-            <div className="text-4xl font-bold">00</div>
-            <div className="text-xl font-bold">DAYS</div>
-          </div>
-          <div className="ml-5 w-12">
-            <div className="text-4xl font-bold">00</div>
-            <div className="text-xl font-bold">HRS</div>
-          </div>
-          <div className="ml-5 w-12">
-            <div className="text-4xl font-bold">00</div>
-            <div className="text-xl font-bold">MIN</div>
-          </div>
-          <div className="ml-5 w-12">
-            <div className="text-4xl font-bold">00</div>
-            <div className="text-xl font-bold">SEC</div>
-          </div>
           <div className="ml-auto flex flex-nowrap">
-            <a href="http://twitter.com"><FaTwitter className="ml-3 mt-3" size={40} /></a>
-            <a href="http://facebook.com"><FaFacebook className="ml-3 mt-3" size={40} /></a>
-            <a href="http://discord.org"><FaDiscord className="ml-3 mt-3" size={40} /></a>
+            <a href="http://twitter.com/esoverse"><FaTwitter className="ml-3 mt-3" size={40} /></a>
+            <a href="https://www.facebook.com/esoverse.esoteric"><FaFacebook className="ml-3 mt-3" size={40} /></a>
+            <a href="https://discord.gg/5JcTCKUZ"><FaDiscord className="ml-3 mt-3" size={40} /></a>
           </div>
         </div>
       </div>
 
       <div className="bg-hero bg-cover">
-        <div className="bg-opacity-80 bg-blue-600 pb-10">
+        <div className="bg-opacity-80 bg-orange-300 pb-10">
           {/* eslint-disable-next-line */}
           <a name="Content" />
           <div className="containerr px-10 mx-auto w-full">
             <Nav />
-            <div className="w-1/2 text-left select-none font-raisin text-7xl text-white"> Welcome </div>
-            <div className="w-1/2 text-left select-none font-raisin text-2xl mt-5 text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br /><br />
-              <a href="#Mint" className="px-4 py-2 shadow-2xl drop-shadow-2xl border-2 border-gray-900 bg-yellow-500 rounded ml-3">Mint Now!</a>
+            <div className="w-1/2 text-left select-none font-quicksand text-7xl text-red-500"> Entrée Esoverse </div>
+            <div className="w-1/2 text-left select-none font-lato text-2xl mt-5 text-black">Mint your own tunnel necks!<br /><br />
+              <a href="#Mint" className="px-4 py-2 shadow-2xl drop-shadow-2xl border-2 border-gray-50 bg-red-500 rounded ml-3">Mint Now!</a>
             </div>
           </div>
         </div>
       </div>
 
       {/* Team */}
-      <div className="bg-booyah bg-cover font-roboto text-white">
-        <div className="bg-opacity-60 bg-gray-800 py-10">
+      <div className="bg-booyah bg-cover font-lato text-white">
+        <div className="bg-opacity-100 bg-lightcoral-800 py-10">
           {/* eslint-disable-next-line */}
           <a name="Team" />
           <Reveal type={<Fade left />} className="container mx-10 mx-auto w-full">
-            <div className="text-white text-5xl">The Team</div>
-            <div className="flex justify-center flex-row m-auto text-white font-bold text-lg">
+            <div className="text-white text-5xl">OG Tunnel Necks</div>
+            <div className="flex justify-center flex-row m-auto text-white font-black-900 text-lg">
               <div className="w-96 bg-1 m-2 p-2 rounded">
-                <img alt="NFT" className="shadow-lg mb-5 border border-black rounded max-w-full h-auto align-middle" src={nfts[0].image} />
-                <p>Tommy</p>
-                <p>Artwork</p>
+                <img alt="NFT" className="shadow-lg mb-5 border border-slate-50 rounded max-w-full h-auto align-middle" src={nfts[1].image} />
+                <p>TunnelNeck</p>
+                <p>Pappa</p>
 
               </div>
               <div className="w-96 bg-1 m-2 p-2 rounded">
-                <img alt="NFT" className="shadow-lg mb-5 border border-black rounded max-w-full h-auto align-middle" src={nfts[1].image} />
-                <p>Dick</p>
-                <p>Geek</p>
+                <img alt="NFT" className="shadow-lg mb-5 border border-slate-50 rounded max-w-full h-auto align-middle" src={nfts[2].image} />
+                <p>TunnelNeck</p>
+                <p>Mamma</p>
               </div>
               <div className="w-96 bg-1 m-2 p-2 rounded">
-                <img alt="NFT" className="shadow-lg mb-5 border border-black rounded max-w-full h-auto align-middle" src={nfts[2].image} />
-                <p>Sally</p>
-                <p>Shill</p>
+                <img alt="NFT" className="shadow-lg mb-5 border border-slate-50 rounded max-w-full h-auto align-middle" src={nfts[3].image} />
+                <p>TunnelNeck</p>
+                <p>AbhiSana</p>
               </div>
             </div>
           </Reveal>
@@ -125,36 +108,36 @@ function App() {
         </div>
       </div>
       {/* Roadmap */}
-      <div className="bg-black text-white">
+      <div className="bg-red-100 text-white">
         <div className="containerr mx-10 mx-auto w-full pt-2 pb-5">
           {/* eslint-disable-next-line */}
           <a name="Roadmap" />
-          {roadmap.map(goal => <Reveal key={goal.step} className="bg-black pt-10 flex">
-            <div className="bg-gray-900 z-0 w-5/6 m-auto rounded mt-10 text-white">
-              <div className="mx-10 p-5 pb-0 flex">
-                <div className="text-2xl mr-3 text-pink-600 text-bold text-mono">{goal.step}</div> <div className="text-2xl text-bold text-mono">{goal.title}</div><br />
+          {roadmap.map(goal => <Reveal key={goal.step} className="bg-red-500 pt-10 flex">
+            <div className="bg-red-500 z-0 w-5/6 m-auto rounded-lg mt-5 text-white">
+              <div className="mx-5 p-5 pb-0 flex">
+                <div className="text-2xl mr-3 text-rose-50 text-bold text-mono">{goal.step}</div> <div className="text-2xl text-bold text-mono">{goal.title}</div><br />
               </div>
-              <div className="mx-10 p-5 flex">
+              <div className="mx-5 p-5 flex">
                 <div className="text-lg text-bold text-mono text-left">{goal.text}</div>
               </div>
             </div>
           </Reveal>)}
         </div>
       </div>
-      {/* Mint */}
-      <Mint/>
+      
+    
 
 
       {/* Tokens */}
       <div className="bg-booyah bg-cover">
-        <div className="bg-opacity-60 bg-gray-800">
+        <div className="bg-opacity-100 bg-purple-900">
           {/* eslint-disable-next-line */}
           <a name="Tokens" />
           <Reveal type={<Fade left />} from={{ y: 1800 }} to={{ y: 0 }} className="containerr mx-10 mx-auto w-full">
             <div className="flex justify-center flex-row m-auto text-white font-bold text-lg">
               {nfts.slice(0, 5).map(nft => <div key={nft.name} className="w-72 bg-1 m-2 p-2 rounded">
                 {nft.name}
-                <img alt="NFT" className="shadow-lg  border border-black rounded max-w-full h-auto align-middle" src={nft.image} />
+                <img alt="NFT" className="shadow-lg  border border-slate-50 rounded max-w-full h-auto align-middle" src={nft.image} />
               </div>)}
             </div>
           </Reveal>
@@ -162,7 +145,7 @@ function App() {
             <div className="flex justify-center flex-row m-auto text-white font-bold text-lg">
               {nfts.slice(5, 10).map(nft => <div key={nft.name} className="w-72 bg-1 m-2 p-2 rounded">
                 {nft.name}
-                <img alt="NFT" className="shadow-lg  border border-black rounded max-w-full h-auto align-middle" src={nft.image} />
+                <img alt="NFT" className="shadow-lg  border border-slate-50 rounded max-w-full h-auto align-middle" src={nft.image} />
               </div>)}
             </div>
           </Reveal>
@@ -170,7 +153,7 @@ function App() {
             <div className="flex justify-center flex-row m-auto text-white font-bold text-lg">
               {nfts.slice(10, 15).map(nft => <div key={nft.name} className="w-72 bg-1 m-2 p-2 rounded">
                 {nft.name}
-                <img alt="NFT" className="shadow-lg  border border-black rounded max-w-full h-auto align-middle" src={nft.image} />
+                <img alt="NFT" className="shadow-lg  border border-slate-50 rounded max-w-full h-auto align-middle" src={nft.image} />
               </div>)}
             </div>
           </Reveal>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,13 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        hero: "url('https://images.unsplash.com/photo-1498036882173-b41c28a8ba34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80')",
-        booyah: "url('https://images.unsplash.com/photo-1542695556-06d09b331678?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')",
-        giraffe: "url('https://images.unsplash.com/photo-1548506446-86bfddac5620?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80')"
+        hero: "url('https://blogger.googleusercontent.com/img/a/AVvXsEg9ZT4-BZDg41-P0MX88uqgbnmyZvdPH_KlL3l5kUHIoIR-L69V3fOIcFG8HX3XMygmRbc8W430c2xpzZZU4FzAO1xo4HJE5P2NY6dgi4Yc55r0MGJHRhjRkPWS9xEfX3t2JCJkrtDvuIcsx0KKLav6Z8s5BwxVQ9KOhXfud7FGqHasliReV8WJ9BC5=s1440')",
+        booyah: "url('https://blogger.googleusercontent.com/img/a/AVvXsEiEJquhv1GSnSk-tKgHzYK44bRSMy4488GshA4Hr0z28GoPRCZqIfj3mgzJRFzXIimzLzFF1ogmr8lh_XN293ZZNM6rOtxC0KaBP6Up-1XyfJ6YgdLaLubm3OOi5fCWTyblmjkWWupwfSE3vcA_6kqsJwU8_5iv9B76ZW915aywU5IIj7wodN6Vl4rb=s1440')",
+        giraffe: "url('https://blogger.googleusercontent.com/img/a/AVvXsEh4DCyIila458woWcXlWWvB1jCtXsKmGU15m5gy2QxJ7cwqJnmdHSfucBCTUqg8vA364EKjd0kWNtlAuHD3QqH1c_T03XpJnHJajzMQop-552JHv3xeDkyPBSphrK2YENUihApwF7WT3LxRgPqZ-TEBzFRYIgesgx6NVjcHaI9_9lISoeUyPtBi0Emu=s1440')"
        },
        fontFamily: {
-        raisin: ['"Rum Raisin"'],
-        roboto: ["Roboto"],
+        quicksand: ['"Quicksand"'],
+        lato: ["Lato"],
       }
     },
   },
